b89ab60ecca2efeddd6f88317813a0d94eef2f56
aine_Jeong <mydream9612@naver.com>
2021-07-19 16:54:00 +0900

    10951,10952 / sc.hasNextInt() 이용하기

---
+++ 백준알고리즘/Main10951.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+ 
+public class Main10951 {
+ 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A,B;
+		
+		while(sc.hasNextInt()) {
+			A = sc.nextInt();
+			B = sc.nextInt();
+			System.out.println(A+B);
+		}
+		
+	}
+}
+ 

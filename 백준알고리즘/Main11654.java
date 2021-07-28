690e689def5b61fa98099edb3975a0a756ba5e48
aine_Jeong <mydream9612@naver.com>
2021-07-29 02:01:32 +0900

    11654번, 아스키코드

---
+++ 백준알고리즘/Main11654.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main11654 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		char x = sc.nextLine().charAt(0);
+		int answer = (int)x;
+		
+		System.out.println(answer);
+		
+	}
+}
+ 
